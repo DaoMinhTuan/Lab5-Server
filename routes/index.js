7bba48c23c7d728bce568578ab427e8795f72259
daoTuan <minhtuan080722@gmail.com>
2022-07-28 10:07:26 +0700

    up project

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        //Dua file vao thu muc uploads
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb) {
+        // de nguyen ten file khong thay doi
+        cb(null, file.originalname);
+    },
+});
+
+var upload = multer({storage: storage});
+var router = express.Router();
+router.post('/upload', upload.array("file",10),
+    function (req, res,next) {
+    var titlename =req.body.title;
+    var contentname = req.body.content;
+    var filename = req.files.filename;
+        console.log(filename);
+        res.send('Upload file thanh cong '+filename+' anh '+titlename+''+contentname);
+    })
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+module.exports = router;
